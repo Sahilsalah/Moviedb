47221979d37c40caeb846f271ae56f4f44f07877
sahil <sahilsalah92@gmail.com>
2019-04-29 14:26:05 +0400

    added the movie page with actor images

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -3,9 +3,13 @@ import './Actor.css'
 import { IMAGE_BASE_URL } from '../../../config'
 
 const Actor = props => {
+  const POSTER_SIZE = 'w154'
+
   return (
-    <div>
-      <h1>Actor Component</h1>
+    <div className="rmdb-actor ">
+      <img src={props.actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.actor.profile_path}` : './images/no-image.jpg'} alt="actor-images" />
+      <span className="rmdb-actor-name">{props.actor.name}</span>
+      <span className="rmdb-actor-character">{props.actor.character}</span>
     </div>
   )
 }
--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import './MovieInfoBar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { calcTime, convertMoney } from '../../../helpers'
 
 const MovieInfoBar = props => {
   return (
-    <div>
-      <h1>Movie Info Bar</h1>
+    <div className="rmdb-movieinfobar">
+      <div className="rmdb-movieinfobar-content">
+        <div className="rmdb-movieinfobar-content-col">
+          <span className="rmdb-movieinfobar-info ">
+            <i class="far fa-clock" />
+            Total Running Time :{calcTime(props.time)}
+          </span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+          <span className="rmdb-movieinfobar-info ">
+            <i class="far fa-money-bill-alt" />
+            Total Budget : {convertMoney(props.budget)}
+          </span>
+        </div>
+        <div className="rmdb-movieinfobar-content-col">
+          <span className="rmdb-movieinfobar-info ">
+            <i class="fas fa-ticket-alt" />
+            Box Office :{convertMoney(props.revenue)}
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
